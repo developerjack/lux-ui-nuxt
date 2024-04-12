17824117616a844dd38b2950aef8b75332a78ff2
liyh@iocharger.com <liyh@iocharger.com>
2024-04-12 18:23:58 +0800

    修改主题和Logo

--- components/app/main-sidebar/MainSiderbar.vue
+++ components/app/main-sidebar/MainSiderbar.vue
@@ -24,21 +24,21 @@ const navigation = ref(configs.mainMenu);
     <!---Top Area -->
     <!-- ---------------------------------------------- -->
 
-    <!-- <template v-slot:prepend>
-      <v-card height="100" class="d-flex align-center justify-center">
-        <img width="160" src="@/assets/images/logo.png" alt="" />
-      </v-card>
-    </template> -->
     <template v-slot:prepend>
-      <RouterLink to="/">
-        <v-card height="100" class="logo-card">
-          <h1 class="logo-text h-full">
-            <Icon class="mr-2" width="40" icon="solar:plain-bold-duotone" />
-            <span>Lux-UI</span>
-          </h1>
-        </v-card>
-      </RouterLink>
+      <v-card height="100" class="d-flex align-center justify-center">
+        <img width="160" src="@/assets/images/logo.svg" alt="" />
+      </v-card>
     </template>
+<!--    <template v-slot:prepend>-->
+<!--      <RouterLink to="/">-->
+<!--        <v-card height="100" class="logo-card">-->
+<!--          <h1 class="logo-text h-full">-->
+<!--            <Icon class="mr-2" width="40" icon="solar:plain-bold-duotone" />-->
+<!--            <span>Lux-UI</span>-->
+<!--          </h1>-->
+<!--        </v-card>-->
+<!--      </RouterLink>-->
+<!--    </template>-->
 
     <!-- ---------------------------------------------- -->
     <!---Menu Area -->
