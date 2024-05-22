61dae3f6e543468eb77e7941d5805f60af4fef19
wht2415486475 <2415486475@qq.com>
2024-05-22 17:34:12 +0800

    feat: 新增弹窗

--- pages/cpo/tariff/index.vue
+++ pages/cpo/tariff/index.vue
@@ -1,35 +1,9 @@
 <template>
 	<yhlx-main-container>
 		<template v-slot:append>
-			<DialogAdd location="Toolbar" @openDialog="openDialog" :demoList="demoList" @removeItem="removeItem(index)"/>
+			<DialogAdd location="Toolbar" @openDialog="openDialog" :demoList="demoList" @removeItem="removeItem"/>
 		</template>
-		<v-dialog v-model="dialog" max-width="800" max-height="600">
-			<v-card class="overflow-auto">
-				<v-container>
-					<v-card-text>
-						<v-row class="d-flex pa-4">
-							<v-col cols="12" sm="12" style="display: flex" v-for="item in restrictionsList" :key="item.label">
-								<v-checkbox-btn
-								v-model="item.isAble"
-								class="pe-2"
-								></v-checkbox-btn>
-								<v-text-field	
-								:disabled="!item.isAble"
-								:label="item.label"
-								v-model="item.value"
-								hide-details
-								></v-text-field>
-							</v-col>
-						</v-row>
-					</v-card-text>
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<yhlx-btn business-type="Close" variant="text" @click="dialog = false">Close</yhlx-btn>
-						<yhlx-btn business-type="Primary" @click="save">Save</yhlx-btn>
-					</v-card-actions>
-				</v-container>
-			</v-card>
-		</v-dialog>
+		<yhlx-reservation-dialog :dialog="ReDialog" @save="save" @close="close"></yhlx-reservation-dialog>
 		<v-data-table :headers="headers" :items="items" @click:row="rowClick"/>
 	</yhlx-main-container>
 </template>
@@ -37,11 +11,12 @@
 <script setup lang="ts">
 import DialogAdd from './Add.vue';
 import YhlxMainContainer from "@/components/container/YhlxMainContainer.vue";
+import YhlxReservationDialog from "@/components/dialog/YhlxReservationDialog.vue";
 import YhlxBtn from "@/components/common/YhlxBtn.vue";
 import axios from "axios";
 import { Random } from 'mockjs';
 const router = useRouter();
-const dialog = ref(false)
+const ReDialog = ref(false)
 const headers = ref([
 	{ title: "Name", key: "name" },
 	{ title: "Min", key: "min" },
@@ -52,34 +27,36 @@ const headers = ref([
 	{ title: "Status", key: "status" },
 	{ title: "Notes", key: "notes" },
 ]);
-const restrictionsList = reactive([{label:'Day',value:'', isAble:false},
-	{label:'Time',value:'', isAble:false },
-	{label:'Energy',value:'', isAble:false },
-	{label:'Duration',value:'', isAble:false },
-	{label:'Current',value:'', isAble:false },
-	{label:'Power',value:'', isAble:false },
-	{label:'Reservation',value:'', isAble:false },
-	{label:'Energy Price',value:'', isAble:false },
-	{label:'Flat Price',value:'', isAble:false },
-	{label:'ParkingTime Price',value:'', isAble:false },
-	{label:'Time Price',value:'', isAble:false },
+const demoList = ref([])
+demoList.value.push([{label:'Day',value:'Monday'},
+	{label:'Time',start:'0:00',end:'11:00', suffix:'' },
+	{label:'Energy',start:'0',end:'20', suffix:'kWh' },
+	{label:'Duration',start:'0',end:'1', suffix:'hour' },
+	{label:'Current',start:'0',end:'32', suffix:'A' },
+	{label:'Power',start:'0',end:'14', suffix:'kW' },
+	{label:'Reservation',start:'Reservation', end:'Expires', suffix:'hour' },
+	{label:'Energy Price',value:'1.2', suffix:'CNY/kWh' },
+	{label:'Flat Price',value:'10', suffix:'CNY' },
+	{label:'ParkingTime Price',value:'5', suffix:'CNY/hour' },
+	{label:'Time Price',value:'20', suffix:'CNY/hour' },
 ])
-const demoList = reactive([])
+onMounted(()=>{
+	console.log('demoList',demoList)
+})
 const items = ref([]);
-axios.get('/api/cpo/tariff').then(response => {
-	items.value = response.data.data.content;
-});
-const save = ()=> {
-	console.log(restrictionsList)
-	demoList.push(restrictionsList)
-	
-	dialog.value = false
+const save = (value)=> {
+	demoList.value.push(value.map(obj => ({ ...obj })))
+	ReDialog.value = false
 }
 const openDialog = ()=> {
-	dialog.value = true
+	ReDialog.value = true
 }
 const removeItem = (index)=> {
-	demoList.splice(index,1)
+	console.log(index)
+	demoList.value.splice(index,1)
+}
+const close = ()=>{
+	ReDialog.value = false
 }
 function rowClick(event: PointerEvent, { item }) {
 	router.push(`tariff/${item.id}`);
