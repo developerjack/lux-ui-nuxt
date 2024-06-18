8ec92aa46273030926f4418cedf9a849d8826c2a
wht2415486475 <2415486475@qq.com>
2024-06-18 11:17:48 +0800

    feat: 拉取代码

--- pages/cpo/location/Add.vue
+++ pages/cpo/location/Add.vue
@@ -3,22 +3,55 @@
 		<template v-slot:content>
 			<v-row>
 				<v-col cols="12" sm="6">
-					<yhlx-select label="eMSP*" :items="operatorNameEnums" />
+					<yhlx-text-field label="Name" />
 				</v-col>
 				<v-col cols="12" sm="6">
-					<yhlx-text-field label="Contact" />
+					<yhlx-text-field label="Opening Times" />
 				</v-col>
 				<v-col cols="12" sm="6">
-					<yhlx-text-field label="Contact Email" />
+					<yhlx-text-field label="Postal Code" />
 				</v-col>
 				<v-col cols="12" sm="6">
-					<yhlx-text-field label="Contact Phone" />
+					<yhlx-select label="Published" :items="['yes','no']" />
 				</v-col>
 				<v-col cols="12" sm="6">
-					<yhlx-text-field label="Valid From*" type="date" />
+					<v-autocomplete
+						variant="outlined"
+						label="State"
+						v-model="selectedProvince"
+						:items="province"
+						@update:search="handleProvinceChange"
+					></v-autocomplete>
 				</v-col>
 				<v-col cols="12" sm="6">
-					<yhlx-text-field label="Valid To" type="date" />
+					<yhlx-select label="City" :items="cities" />
+				</v-col>
+				<v-col cols="12" sm="6">
+					<yhlx-text-field label="Address" />
+				</v-col>
+				<v-col cols="12" sm="6">
+					<yhlx-text-field label="Directions" />
+				</v-col>
+				<v-col cols="12" sm="6">
+					<yhlx-text-field label="EVSE Number" type="number"/>
+				</v-col>
+				<v-col cols="12" sm="6">
+					<v-combobox
+					variant="outlined"
+					v-model="select"
+					:items="items"
+					label="Facilities"
+					chips
+					clearable
+					multiple
+					/>
+				</v-col>
+				<v-col cols="12" sm="12">
+					<yhlx-text-field label="Note" />
+				</v-col>
+				<v-col cols="12" sm="12" class="font-weight-bold">Location:  </v-col><!--<span>{{Location}}</span>-->
+				<v-col cols="12" sm="12">
+					<map-container @getLocationName="getLocationName" :height="400"></map-container>
 				</v-col>
 			</v-row>
 		</template>
@@ -29,5 +62,28 @@
 import YhlxAddDialog from "@/components/dialog/YhlxAddDialog.vue";
 import YhlxTextField from "@/components/common/YhlxTextField.vue";
 import YhlxSelect from "@/components/common/YhlxSelect.vue";
-import { operatorNameEnums } from "@/data/data";
+import MapContainer from '@/components/common/MapContainer.vue'
+import { operatorNameEnums,ChineseProvince } from "@/data/data";
+const select = ref(null)
+const province = ref([])
+const items = ref(['Hotel','Restaurant','Cafe','Mall','Supermarket','Sport','Recreation Area','Nature','Museum','Bus Stop','Taxi Stand','Train Station','Airport','Carpool Parking','Fuel Station','Wi-Fi'])
+let cities = ref([])
+const selectedProvince = ref()
+let selectedCity = ref()
+const published = ref()
+const status = ref()
+const handleProvinceChange = () => {
+	selectedCity.value = ''
+	cities.value = ChineseProvince.find(item=>item.province == selectedProvince.value).cities
+	console.log()
+}
+const Location = ref('')
+const getLocationName = (selectedAddress) => {
+	Location.value = selectedAddress
+}
+onMounted(()=>{
+	ChineseProvince.forEach(element => {
+		province.value.push(element.province)
+	});
+})
 </script>
